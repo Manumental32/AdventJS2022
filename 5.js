1b8afd23f28190ef1bdf4eb600a6c9990f36013d
Manuel Agustin Aquino <manuel.aquino.utn@gmail.com>
2022-12-06 16:00:11 -0300

    89

--- 5.js
+++ 5.js
@@ -1,24 +1,28 @@
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
-    let response = 0;
-    let cities = 0;
-    let available = maxGifts - response;
+    let combinaciones = [];
+    combinaciones.push([], [giftsCities[0]])
 
-    let index = 0
-    function test(a,b) {
-        index += 1;
-        return index % 2 ? (b - a) : (a - b); 
-    }
-    giftsCities.sort((a,b)=> test(a,b));
-    giftsCities.forEach(g => {
-        if(cities < maxCities && available >= g && g <= maxGifts) {
-            response += g
-            cities++;
-            available = maxGifts - response;
-        }
+    giftsCities.map(x => {
+        const newList = combinaciones.map(combinacion => {
+            let _combinacion = [...combinacion]
+            console.log('_combinacion',_combinacion)
+            if(_combinacion.length < maxCities) {
+                _combinacion.push(x)
+            }
+            return _combinacion
+        })
+        combinaciones = combinaciones.concat(newList)
     })
-    console.log('response', response)
-    return response;
+    combinaciones.shift()
+
+    return Math.max(
+        ...combinaciones.map( x => {
+            let sum = x.reduce((total, num)=> total + num)
+            return sum > maxGifts ? 0 : sum
+        })
+    )
 }
+
 // la suma más alta de regalos a repartir
 // visitando un máximo de 3 ciudades
 // es de 20: [12, 3, 5]
@@ -29,9 +33,9 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
 
 // getMaxGifts([50], 100, 1) // 50
 
-getMaxGifts([1, 2, 1], 2, 2) // 20
+// getMaxGifts([1, 2, 1], 4, 7) // 20
 
-// getMaxGifts([12, 3, 11, 5, 7], 20, 3) // 20
+console.log('getMaxGifts',getMaxGifts([12, 3, 11, 5, 7], 20, 3)) // 20
 // getMaxGifts([50], 15, 1) // 0
 // getMaxGifts([50], 100, 1) // 50
 // getMaxGifts([50, 70], 100, 1) // 70
