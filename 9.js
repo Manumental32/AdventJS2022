b04c486d0f4b70c012074156697b7dcf62dc2c93
Manuel Agustin Aquino <manuel.aquino.utn@gmail.com>
2022-12-12 12:09:46 -0300

    300
    contando el maximo posible de 0s a la izquierda de un 1

--- 9.js
+++ 9.js
@@ -1,24 +1,12 @@
 function countTime(leds) {
-    let response = 0;
-    function getTime(leds) {
-        let auxLeds = [...leds]
-        for (let index = 0; index < auxLeds.length; index++) { 
-            // let prevValue = (leds[index+1] === undefined) ? leds[0] : (leds[index+1]);
-            // console.log('prevValue',prevValue);
-            auxLeds[index] = ((leds[index+1] === undefined) ? leds[0] : (leds[index+1]) === 1) ? 1 : auxLeds[index];
-        }
-        response++;
-        return auxLeds
-    }
-
-    while(leds.some(e => (e === 0))){
-        leds = getTime(leds);
-    }
-    // console.log('response f',response)
-    return response * 7;
+    leds = leds.join('').split(1);
+    leds[0] += leds.pop();
+    let count = Math.max(...leds.map(led =>led.length));
+    return count * 7;
 }
   
 //   countTime([0, 0, 0, 1]) // 21
 //   countTime([0, 0, 1, 0, 0]) // 28
 //   countTime([1, 0, 0, 1, 0, 0]) // 14
-  countTime([1, 1, 0, 0, 0, 0]) // 28
+//   countTime([1, 1, 0, 0, 0, 0]) // 28
+countTime([0, 1, 0, 0, 1, 0, 0, 0]) // 28
