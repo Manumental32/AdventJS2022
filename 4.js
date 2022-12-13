c64a8acc5b69ba16b0e421a74e92ef71fdc8e002
Manuel Agustin Aquino <manuel.aquino.utn@gmail.com>
2022-12-13 12:09:18 -0300

    130
    cambio el for por un every que compara cada valor de la caja previa con los de la actual

--- 4.js
+++ 4.js
@@ -1,25 +1,26 @@
 function fitsInOneBox(boxes) {
-    boxes.sort((a, b) => {
-        return a.l - b.l || a.w - b.w || a.h - b.h;
-    });
-    return sortedBox(boxes)
+    return boxes.sort((a, b) => {
+        return (a.l + a.w + a.h) - (b.l + b.w + b.h);
+    }).every((box,i) => {
+      if(i === 0) return true;
+      const prev = boxes[i - 1];
+      return box.l > prev.l && box.w > prev.w && box.h > prev.h
+    })
 }
 
-function sortedBox(boxes){
-  let second_index;
-	for(let first_index = 0; first_index <= boxes.length; first_index++){
-  	  second_index = first_index + 1;
-      if(boxes[second_index] && (boxes[second_index].l - boxes[first_index].l <= 0)) return false;
-      if(boxes[second_index] && (boxes[second_index].w - boxes[first_index].w <= 0)) return false;
-      if(boxes[second_index] && (boxes[second_index].h - boxes[first_index].h <= 0)) return false;
-  }
-  return true;
-}
+fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 }
+]) //false
 
-const boxes = [
-    { l: 1, w: 1, h: 1 },
-    { l: 2, w: 2, h: 2 },
-    { l: 2, w: 10, h: 2}
-]
-  
-console.log(fitsInOneBox(boxes)) // false
+fitsInOneBox([
+  { l: 1, w: 1, h: 10 },
+  { l: 3, w: 3, h: 12 },
+  { l: 2, w: 2, h: 1 },
+]) //false
+
+fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 }
+]) //true
